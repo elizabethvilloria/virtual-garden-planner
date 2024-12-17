7f06dce6394f25401fdaead9ecd06099f61620a5
elizabethvilloria <lizvilloria@gmail.com>
2024-12-17 09:59:45 +0100

    Add loading state handling

--- src/components/GardenGrid.js
+++ src/components/GardenGrid.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './GardenGrid.css';
 
-const GardenGrid = ({ grid, onPlantPlacement }) => {
-    return (
-      <div className="garden-grid">
-        {grid.map((row, rowIndex) =>
-          row.map((cell, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              className={`grid-cell ${cell ? 'occupied' : 'empty'}`}
-              onClick={() => onPlantPlacement(rowIndex, colIndex)}
-            >
-              {cell}
-            </div>
-          ))
-        )}
-      </div>
-    );
-  };
-g  
+const GardenGrid = ({ grid, onPlantPlacement, isLoading }) => {
+  if (isLoading) {
+    return <div className="loading-state">Loading garden grid...</div>;
+  }
+
+  return (
+    <div className="garden-grid">
+      {grid.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            className="grid-cell"
+            onClick={() => onPlantPlacement(rowIndex, colIndex)}
+          >
+            {cell}
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+GardenGrid.propTypes = {
+  grid: PropTypes.arrayOf(PropTypes.array).isRequired,
+  onPlantPlacement: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool,
+};
+
+GardenGrid.defaultProps = {
+  isLoading: false,
+};
 
 export default GardenGrid;
