305fd3817998a356296513aae20022bb0ddd5ec8
elizabethvilloria <lizvilloria@gmail.com>
2024-12-17 09:57:15 +0100

    Debugging

--- src/components/GardenGrid.js
+++ src/components/GardenGrid.js
@@ -2,22 +2,21 @@ import React from 'react';
 import './GardenGrid.css';
 
 const GardenGrid = ({ grid, onPlantPlacement }) => {
-    return (
-      <div className="garden-grid">
-        {grid.map((row, rowIndex) =>
-          row.map((cell, colIndex) => (
-            <div
-              key={`${rowIndex}-${colIndex}`}
-              className="grid-cell"
-              onClick={() => onPlantPlacement(rowIndex, colIndex)}
-            >
-              {cell}
-            </div>
-          ))
-        )}
-      </div>
-    );
-  };
-g  
+  return (
+    <div className="garden-grid">
+      {grid.map((row, rowIndex) =>
+        row.map((cell, colIndex) => (
+          <div
+            key={`${rowIndex}-${colIndex}`}
+            className="grid-cell"
+            onClick={() => onPlantPlacement(rowIndex, colIndex)}
+          >
+            {cell}
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
 
 export default GardenGrid;
