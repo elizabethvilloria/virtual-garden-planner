cda80d8e5471a0e9f7e074f82b86757b7616a1a7
elizabethvilloria <lizvilloria@gmail.com>
2024-12-17 09:58:21 +0100

    Feat: Add PropTypes validations for GardegGrid component

--- src/components/GardenGrid.js
+++ src/components/GardenGrid.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './GardenGrid.css';
 
 const GardenGrid = ({ grid, onPlantPlacement }) => {
-  return (
-    <div className="garden-grid">
-      {grid.map((row, rowIndex) =>
-        row.map((cell, colIndex) => (
-          <div
-            key={`${rowIndex}-${colIndex}`}
-            className="grid-cell"
-            onClick={() => onPlantPlacement(rowIndex, colIndex)}
-          >
-            {cell}
-          </div>
-        ))
-      )}
-    </div>
-  );
+    return (
+      <div className="garden-grid">
+        {grid.map((row, rowIndex) =>
+          row.map((cell, colIndex) => (
+            <div
+              key={`${rowIndex}-${colIndex}`}
+              className="grid-cell"
+              onClick={() => onPlantPlacement(rowIndex, colIndex)}
+            >
+              {cell}
+            </div>
+          ))
+        )}
+      </div>
+    );
+  };
+
+GardenGrid.propTypes = {
+  grid: PropTypes.arrayOf(PropTypes.array).isRequired,
+  onPlantPlacement: PropTypes.func.isRequired,
 };
 
 export default GardenGrid;
